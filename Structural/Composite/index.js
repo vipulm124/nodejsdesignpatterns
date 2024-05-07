591f60d65aeef5cc43e0c08ddf4e5057081a0b00
Vipul Malhotra <vipulmalhotra@192.168.1.7>
2024-05-08 00:10:22 +0530

    added composite  pattern

---
+++ Structural/Composite/index.js
@@ -0,0 +1,22 @@
+var CatalogItem = require('./CatalogItem');
+var CatalogGroup = require('./CatalogGroup');
+
+
+var boots = new CatalogItem("Leather Boots", 79.99);
+var sneakers = new CatalogItem("Kicks", 39.99);
+var flipFlops = new CatalogItem("California work boots", 19.99);
+
+var group_shoes = new CatalogGroup("Shoes and Such", [boots, sneakers, flipFlops])
+
+var group_food = new CatalogGroup("Food for while you try on clothes", [
+    new CatalogItem("Milkshake", 5.99),
+    new CatalogItem("French Fries", 3.99)
+])
+
+var keychain = new CatalogItem("Key Chain", .99)
+
+var catalog = new CatalogGroup("Clothes and Food", [keychain, group_food, group_shoes]);
+
+console.log('shoes total: ', `$${catalog.total}`);
+
+catalog.print();
