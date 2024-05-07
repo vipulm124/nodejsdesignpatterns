8595ec6b159818235fdf112cf263ca8685b7f524
Vipul Malhotra <vipulmalhotra@192.168.1.7>
2024-05-08 00:32:44 +0530

    added decorator  pattern

---
+++ Structural/Decorator/index.js
@@ -0,0 +1,20 @@
+var Shopper = require('./Shopper');
+var { InventoryItem, GoldenInventoryItem, DiamondInventoryItem } = require('./InventoryItem');
+
+var alex = new Shopper('Alex', 3500);
+
+var walkman = new InventoryItem('Walkman', 29.99);
+var neclace = new InventoryItem('Neclace', 9.99);
+
+
+var gold_neclace = new GoldenInventoryItem(neclace);
+var diamond_gold_neclace = new DiamondInventoryItem(gold_neclace);
+
+var diamond_walkman = new DiamondInventoryItem(walkman);
+
+
+
+alex.purchase(diamond_gold_neclace);
+alex.purchase(diamond_walkman);
+
+alex.printStatus();
